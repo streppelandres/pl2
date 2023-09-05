d726c2b36325ee0783582d8463554e0319a93ea7
Andrés Caballero Streppel <andres.streppel@gmail.com>
2023-09-05 19:15:34 -0300

    I01 exercise

---
+++ exercises/OOP/I01Exercise/ClassLibrary/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary
+{
+    public class Account
+    {
+        private string holder;
+        private decimal amount;
+
+        public Account(string holder, decimal amount)
+        {
+            this.holder = holder;
+            this.amount = amount;
+        }
+
+        public string Show()
+        {
+            return $"Account holder: {this.holder}, amount: ${this.amount}";
+        }
+        
+        public void Add(decimal amount)
+        {
+            if (amount <= 0) return;
+            this.amount += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            this.amount -= amount;
+        }
+    }
+}
